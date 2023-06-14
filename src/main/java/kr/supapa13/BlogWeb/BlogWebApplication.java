55fdd7eaac6c1c83e17b512d34e7a68400dc24ea
DevBori <80079682+DevBori7@users.noreply.github.com>
2023-06-14 16:57:14 +0900

    Initial commit

---
+++ src/main/java/kr/supapa13/BlogWeb/BlogWebApplication.java
@@ -0,0 +1,13 @@
+package kr.supapa13.BlogWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/supapa13/BlogWeb/BlogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.supapa13.BlogWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
